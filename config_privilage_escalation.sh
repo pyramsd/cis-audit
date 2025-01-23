812ccbca6a8e0d0e344f68f26329949290c6afed
pyramsd <minauzum56@gmail.com>
2025-01-23 12:13:47 -0500

    aaaa

--- config_privilage_escalation.sh
+++ config_privilage_escalation.sh
@@ -4,3 +4,16 @@ if dpkg-query -s sudo &>/dev/null; then
 else
     echo -e "\e[31m[-] Sudo no instalado"
 fi
+
+echo -e "\n"
+
+echo -e "\e[34m[*] Los comandos sudo utilizan pty"
+output1=$(grep -rPi -- '^\h*Defaults\h+([^#\n\r]+,\h*)?use_pty\b' /etc/sudoers*)
+output2=$(grep -rPi -- '^\h*Defaults\h+([^#\n\r]+,\h*)?!use_pty\b' /etc/sudoers*)
+exit_code2=$?
+echo $exit_code2 
+if [[ $output1 == "/etc/sudoers:Defaults       use_pty" && $exit_code2 -ne 0 ]]; then
+    echo -e "\e[32m[+] $output1"
+else
+    echo -e "\e[31m[-] $output1 $output2"
+fi
