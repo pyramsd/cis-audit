2387c72389ac24d86b6c9892ee8c275c2d123141
pyramsd <minauzum56@gmail.com>
2025-10-18 00:39:20 -0500

    Actualización 18/07/2025

--- 2_config_cron_permissions.sh
+++ 2_config_cron_permissions.sh
@@ -10,10 +10,10 @@ declare -A cron_paths=(
     ["/etc/cron.d"]="700:root:root"
 )
 
-echo -e "${BLUE}[*] Permisos de los archivos del servicio Cron{RESET}"
+echo -e "${BLUE}[*] Permisos de los archivos del servicio Cron${RESET}"
 if ! dpkg -s cron &>/dev/null; then
-    echo -e "${PINK}[!] Cron no está instalado en el sistema.{RESET}"
-    echo -e "\e[33m[!]Instalar y habilitar Cron si es necesario.{RESET}"
+    echo -e "${PINK}[!] Cron no está instalado en el sistema.${RESET}"
+    echo -e "\e[33m[!]Instalar y habilitar Cron si es necesario.${RESET}"
 else
     echo -e "${GREEN}[+] Cron instalado"
     counter=$((counter + 1))
@@ -23,14 +23,14 @@ else
     cron_activated=$(systemctl is-active cron)
 
     [[ $cron_enabled == "enabled" ]] && {
-        echo -e "\e[32m[+] Cron: $cron_enabled{RESET}"
+        echo -e "\e[32m[+] Cron: $cron_enabled${RESET}"
         counter=$((counter + 1))
-    } || echo -e "${PINK}[!] Cron: $cron_enabled{RESET}"
+    } || echo -e "${PINK}[!] Cron: $cron_enabled${RESET}"
     
     [[ $cron_activated == "active" ]] && {
-        echo -e "\e[32m[+] Cron: $cron_activated{RESET}\n"
+        echo -e "\e[32m[+] Cron: $cron_activated${RESET}\n"
         counter=$((counter + 1))
-    } || echo -e "${PINK}[!] Cron: $cron_activated{RESET}\n"
+    } || echo -e "${PINK}[!] Cron: $cron_activated${RESET}\n"
 
     # Iterar sobre cada archivo y verificar permisos
     for file in "${!cron_paths[@]}"; do
@@ -39,13 +39,13 @@ else
 
         echo "Permisos de $file:"
         if [[ "$actual_permissions" == "$expected_permissions" ]]; then
-            echo -e "\e[32m[+] $actual_permissions{RESET}\n"
+            echo -e "\e[32m[+] $actual_permissions${RESET}\n"
             counter=$((counter + 1))
         else
-            echo -e "${PINK}[-] $actual_permissions \e[33m-> $expected_permissions{RESET}\n"
+            echo -e "${PINK}[-] $actual_permissions \e[33m-> $expected_permissions${RESET}\n"
         fi
     done
 
-    echo -e "\e[33m[!] Habilitar Cron si es necesario.{RESET}"
+    echo -e "\e[33m[!] Habilitar Cron si es necesario.${RESET}"
 
 fi
